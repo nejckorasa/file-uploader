605e27e99f12e69926f2125069dca5ce54356a4f
Nejc Korasa <nejc.korasa@gmail.com>
2018-03-14 08:18:23 +0100

    rename test file

--- src/test/kotlin/com/marand/thinkmed/marsaver/ApplicationTests.kt
+++ src/test/kotlin/com/marand/thinkmed/marsaver/ApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class MarSaverApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {}
